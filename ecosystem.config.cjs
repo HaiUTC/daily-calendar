61c1d608dfad3ecb7a385ee31c3670550736015b
Thanh Hai <sahaetran@gmail.com>
2026-04-28 15:59:18 +0000

    chore: remove log file configurations from PM2 ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -11,8 +11,6 @@ module.exports = {
       env: {
         NODE_ENV: 'production',
       },
-      error_file: 'logs/error.log',
-      out_file: 'logs/out.log',
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
       time: true,
     },
